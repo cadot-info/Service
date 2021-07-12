872aed437fb132e8f7dafe559bbbafa5f7af6501
tt2442 <theokdo@gmail.com>
2021-07-12 10:28:28 +0200

    modify place

---
+++ FileFunctions.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\CMService;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class FileFunctions
+{
+    // pour extraire l'extension d'un fichier
+    public function extension($file): string
+    {
+        return (strtolower(pathinfo($file, PATHINFO_EXTENSION)));
+    }
+
+    // for copy dir with recursivity
+    function copydir($src, $dst)
+    {
+        $dir = opendir($src);
+        @mkdir($dst);
+        while (false !== ($file = readdir($dir))) {
+            if (($file != '.') && ($file != '..')) {
+                if (is_dir($src . '/' . $file)) {
+                    $this->copydir($src . '/' . $file, $dst . '/' . $file);
+                } else {
+                    $this->copy($src . '/' . $file, $dst . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
+    }
+    //remove dir with recursivity
+    function deletedir($dir)
+    {
+        if (!file_exists($dir)) {
+            return true;
+        }
+
+        if (!is_dir($dir)) {
+            return unlink($dir);
+        }
+
+        foreach (scandir($dir) as $item) {
+            if ($item == '.' || $item == '..') {
+                continue;
+            }
+
+            if (!$this->deletedir($dir . DIRECTORY_SEPARATOR . $item)) {
+                return false;
+            }
+        }
+
+        return rmdir($dir);
+    }
+    //move directory with recursivity
+    function movedir($origine, $destination)
+    {
+
+        //création des réperoitre de destination s'il n'existe pas
+        $dir = explode('/', $destination);
+        unset($dir[count($dir) - 1]);
+        $ddir = '';
+        foreach ($dir as $val) {
+            $ddir .= $val . '/';
+            if (!file_exists($ddir)) mkdir($ddir);
+        }
+        rename($origine, $destination);
+    }
+}
---
+++ String_functions.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\CMService;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class String_functions
+{
+    /**
+     * pour extraine un string entre 2 string ou charactères
+     *
+     * @param string $string    chaine
+     * @param string $stringdeb chaine de début
+     * @param string $stringfin chaine de fin
+     * @return string
+     */
+    function chaine_extract($string, $stringdeb, $stringfin)
+    {
+        $deb = strpos($string, $stringdeb);
+        $fin = strpos($string, $stringfin, $deb);
+        return substr($string, $deb + strlen($stringdeb), $fin - $deb - strlen($stringdeb));
+    }
+    // chaine_remplace($html, 'background-image', ')', 'background-image: url({{file' . $file  . '}})');
+    function chaine_remplace($html, $debs, $fins, $chaine, $start = 0)
+    {
+        $pos = strpos($html, $debs, $start);
+        $fin = strpos($html, $fins, $pos + 1);
+        $html = substr($html, $start, $pos) . $chaine . substr($html, $fin + 1);
+    }
+    function extract($str, $pos, $start, $end = '')
+    {
+        if ($end == '') $end = $start;
+        $sub = strpos($str, $start, $pos); //on cherche la position du départ dans la chaine
+        $sub += strlen($start); //on ajoute la longueur de la chaine départ
+        $size = strpos($str, $end, $sub) - $sub; //on calcule la taille de la chaine-le départ
+        return substr($str, $sub, $size); //on retourne la chaine
+    }
+
+
+    function insert($chaine, $strdeb, $insert, $after = true)
+    {
+        if ($after) $pos = strpos((string)$chaine, $strdeb) + strlen($strdeb);
+        else $pos = strpos((string)$chaine, $strdeb);
+        return substr_replace((string)$chaine, $insert, $pos, 0);
+    }
+}
