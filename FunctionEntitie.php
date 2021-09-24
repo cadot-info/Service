337bff65f4148cecb7b3f8d0a7dd501cd0d31736
michael <michael@cadot.info>
2021-09-24 16:30:46 +0100

    pagination

--- FunctionEntitie.php
+++ FunctionEntitie.php
@@ -29,31 +29,22 @@ class FunctionEntitie
 
     // renvoie un tableau réorganiser d'un tableau ou d'un repository complet
     // si pas de donnees, on fait un findall 
-    public function reorder($repository, $donnees = null): array
+    public function reorder($repository, $donnees = null, $limit = 0, $offset = 0): array
     {
-        $array = [];
+        $tab = [];
         //si on a pas données un array on va chercher toutes les données du repository
         if (is_null($donnees))
             $donnees = $this->em->getRepository("App:" . ucfirst($repository))->findall();
         if ($donnees) {
-            //on récupère le trie enregitsré dans la bd
+            //on récupère le trie enregistré dans la bd
             if ($base = $this->em->getRepository("App:CM\Sortable")->findOneBy(['entite' => ucfirst($repository)])) {
-                $sortable = explode(',', $base->getordre()); //tableau des ordres
-                //on liste les tries
-                foreach ($sortable as $index => $num) {
-                    $res =  array_filter(
-                        $donnees,
-                        function ($e) use (&$num) {
-                            return $e->getId() == $num;
-                        }
-                    );
-                    $array[$index] = reset($res);
-                }
-                return $array;
+                $tab = $this->sortArrayObjetByArray($donnees, 'getId', $base->getordre());
             } else {
-                return $donnees;
+                $tab = $donnees;
             }
         } else return [];
+        $retour = $limit ? array_slice($tab, $offset, $limit) : array_slice($tab, $offset);
+        return $retour;
     }
     //funtion qui récupère toutes les données d'un field
     public function getAllOfFields($repository, $field, $removeDoublon = true): string
@@ -66,5 +57,31 @@ class FunctionEntitie
         }
         if ($removeDoublon) return implode(',', array_unique($tabres));
         else return implode(',', $tabres);
+    }
+    /**
+     * Method sortArrayObjetByArray order a array of objetcs by another array or string 
+     *
+     * @param $objets array of objects
+     * @param $function  getId, getName...
+     * @param $array  array or string with the good range
+     *
+     * @return void
+     */
+    public function sortArrayObjetByArray($objets, $function, $array)
+    {
+        $reste = $objets;
+        if (\is_string($array)) {
+            $array = explode(',', $array);
+        }
+        $tab = [];
+        foreach ($array as $num) {
+            foreach ($objets as $key => $value) {
+                if ($value->$function() == intval($num)) {
+                    $tab[] = $objets[$key];
+                    unset($reste[$key]);
+                }
+            }
+        }
+        return array_merge($tab, $reste);
     }
 }
